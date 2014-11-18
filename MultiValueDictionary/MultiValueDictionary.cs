4d421280d85fdc86a4163f4add27375cf6f4fd1f
Martin <martin@happyjazz.eu>
2014-11-18 13:24:38 +0100

    Created Count, Add and Get

---
+++ MultiValueDictionary/MultiValueDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiValueDictionary
+{
+    public class MultiValueDictionary<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, ISet<TValue>> _data = new Dictionary<TKey, ISet<TValue>>();
+
+        public int Count
+        {
+            get { return _data.Count; }
+        }
+
+        public bool Add(TKey key, TValue value)
+        {
+            if (_data.ContainsKey(key))
+            {
+                if (_data[key].Contains(value))
+                {
+                    return false;
+                }
+                _data[key].Add(value);
+                return true;
+            }
+            ISet<TValue> valueSet = new HashSet<TValue>();
+            valueSet.Add(value);
+            _data.Add(new KeyValuePair<TKey, ISet<TValue>>(key, valueSet));
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public ISet<TValue> Get(TKey key)
+        {
+            if (_data.ContainsKey(key))
+            {
+                return _data[key];
+            }
+            throw new Exception("Key not found");
+        }
+    }
+}
--- TestMultiValueDictionary/UnitTest1.cs
+++ TestMultiValueDictionary/UnitTest1.cs
@@ -1,14 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiValueDictionary;
 
 namespace TestMultiValueDictionary
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public static MultiValueDictionary<string, string> mvd = new MultiValueDictionary<String, String>();
+
+        [ClassInitialize]
+        public static void TestInitialize(TestContext context)
         {
+             mvd = new MultiValueDictionary<string, string>();
+        }
+        [TestMethod]
+        public void TestCount()
+        {
+            int result = mvd.Count;
+            Debug.Print(result.ToString());
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void TestAdd()
+        {
+            bool result = mvd.Add("SomeMovie", "192.12.33.4");
+            Debug.Print(result.ToString());
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            String key = "SomeMovie";
+            ISet<String> valueSet = mvd.Get(key);
+            Assert.IsTrue(valueSet.Contains("192.12.33.4"));
         }
     }
 }
